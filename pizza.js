da1e29f1a36f256df7ceb4b961abef6e5d0e8ba8
Patrick-Bonini <83367807+Patrick-Bonini@users.noreply.github.com>
2021-05-04 13:03:38 -0400

    Update pizza.js

--- pizza.js
+++ pizza.js
@@ -1,3 +1,11 @@
-<!DOCTYPE html>
-  <html>
-  <head>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Challenge: A picture-perfect trip</title>
+        <meta charset="utf-8">
+    </head>
+     <body>
+      
+     </body
+</html>
+
