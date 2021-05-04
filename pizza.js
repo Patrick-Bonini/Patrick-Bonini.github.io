e871f9d9a168ac49cffa871287c8b50eaf61bd84
Patrick-Bonini <83367807+Patrick-Bonini@users.noreply.github.com>
2021-05-04 13:05:27 -0400

    Update pizza.js

--- pizza.js
+++ pizza.js
@@ -5,7 +5,16 @@
         <meta charset="utf-8">
     </head>
      <body>
-      
+       <script>
+            var taillepizza = prompt ("Quelle taille de pizza veut-tu? (en pouces)")
+            var surface = parseFloat(((taillepizza/2)**2)*Math.PI)
+            surface = Math.floor(surface * 100) / 100;
+            (surface.toFixed(2));
+            alert ("La surface de ta pizza est " + surface + " pouces.")
+            var boite = parseFloat (surface*2)
+            alert ("Le volume de la boite sera " + boite + " pouces^2") 
+       </script>
+
      </body
 </html>
 
